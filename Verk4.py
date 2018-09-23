33734309dde2e8472f3b2ad7414bfbe69d811ec5
mrelli24 <32517753+mrelli24@users.noreply.github.com>
2018-09-23 14:03:34 +0000

    Add files via upload

---
+++ Verk4.py
@@ -0,0 +1,46 @@
+from sys import argv
+from bottle import *
+import urllib.request, json
+
+"""
+with open("gengi.json","r") as skra:
+    gogn = json.load(skra)
+print(gogn)
+"""
+
+@route("/")
+def index():
+    return """
+    <h1> Verkefni 4 </h1>
+    <a href="/a">Local Json</a> - 
+    <a href="/b">Json API</a>
+    """
+
+
+
+@route("/a")
+def index():
+    return template("index.tpl")
+
+
+with urllib.request.urlopen("http://apis.is/currency/") as url:
+    data = json.loads(url.read().decode())
+
+@route("/b")
+def index():
+    return template("index2.tpl", data = data)
+
+
+
+
+@route("/static/<skra>")
+def static_skra(skra):
+    return static_file(skra, root="./static")
+
+@error(404)
+def villa(error):
+    return "<h1 style = color:red>Þessi síða finnst ekki</h1>"
+
+
+run(host='localhost', port=8080, debug=True)
+#run(host = 'localhost',port= 5000, reloader=True, debug=True))
