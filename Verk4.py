b407b7467915b217e2ab975f31e98828052ff3c2
mrelli24 <32517753+mrelli24@users.noreply.github.com>
2018-10-04 13:43:53 +0000

    Update Verk4.py

--- Verk4.py
+++ Verk4.py
@@ -42,4 +42,4 @@ def villa(error):
     return "<h1 style = color:red>Þessi síða finnst ekki</h1>"
 
 
-run(host='localhost', port=8080, reloader=True,debug=True)
+run(host="0.0.0.0", port=os.environ.get('PORT'))
